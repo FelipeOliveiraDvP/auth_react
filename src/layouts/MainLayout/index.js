7f4998d504f046c35afc37e6b2dfbd47f0d57297
Felipe Oliveira <felipe.oliveirac8@gmail.com>
2020-01-30 13:09:09 -0300

    Refactor the main layout

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -3,6 +3,7 @@ import { Layout } from 'antd'
 
 import Header from '../../components/common/Header'
 import Footer from '../../components/common/Footer'
+import SideMenu from '../../components/common/SideMenu'
 
 const { Content } = Layout
 
@@ -11,11 +12,14 @@ class MainLayout extends Component {
     render() {
         return (
             <Layout className='layout'>
-                <Header />
-                <Content style={{ backgroundColor: '#fff' }}>
-                    {this.props.children}
-                </Content>
-                <Footer />
+                <SideMenu />
+                <Layout>
+                    <Header />                
+                    <Content style={{ backgroundColor: '#fff', padding: '10px' }}>
+                        {this.props.children}
+                    </Content>
+                    <Footer />
+                </Layout>                
             </Layout>
         );
     }
